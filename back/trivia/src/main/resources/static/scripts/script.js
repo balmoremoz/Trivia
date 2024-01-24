d41ab789b42b8c99394f7043442a4ec66a2faaa1
balmore <balmoreavalos2@gmail.com>
2024-01-24 13:15:35 +0100

    movimento de la ficha completo

--- back/trivia/src/main/resources/static/scripts/script.js
+++ back/trivia/src/main/resources/static/scripts/script.js
@@ -3,109 +3,109 @@ let posicionCat = 0;
 let arrayPosiciones = [];
 
 function llamada() {
-  let preguntas = new Array();
-  let xhr = new XMLHttpRequest();
+    let preguntas = new Array();
+    let xhr = new XMLHttpRequest();
 
-  xhr.open('GET', apiServerUrl, true);
+    xhr.open('GET', apiServerUrl, true);
 
-  xhr.onload = function () {
-    if (xhr.status >= 200 && xhr.status < 300) {
-      var responseData = JSON.parse(xhr.responseText);
-      console.log('Resultado:', responseData);
-      preguntas.push(responseData)
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            var responseData = JSON.parse(xhr.responseText);
+            console.log('Resultado:', responseData);
+            preguntas.push(responseData)
 
-    } else {
-      console.error('Error', xhr.statusText);
-    }
-  };
+        } else {
+            console.error('Error', xhr.statusText);
+        }
+    };
 
-  xhr.onerror = function () {
-    console.error('Error');
-  };
-  xhr.send();
+    xhr.onerror = function () {
+        console.error('Error');
+    };
+    xhr.send();
 }
 
-const filas = 6;
-const columnas = 5;
+
 function generarTablero(filas, columnnas) {
 
-  const tablero = document.querySelector('.tablero');
-  tablero.style.gridTemplateColumns = `repeat(${columnnas}, 1fr)`;
-  tablero.style.gridTemplateRows = `repeat(${filas}, 1fr)`;
-  const arrayCat = ['G', 'H', 'D', 'C', 'A', 'E'];
+    const tablero = document.querySelector('.tablero');
+    tablero.style.gridTemplateColumns = `repeat(${columnnas}, 1fr)`;
+    tablero.style.gridTemplateRows = `repeat(${filas}, 1fr)`;
+    const arrayCat = ['G', 'H', 'D', 'C', 'A', 'E'];
 
-  for (let i = 0; i < filas; i++) {
-    arrayPosiciones[i] = []
-  }
-
-  for (let j = 0; j < columnnas; j++) {
-
-    if (posicionCat == arrayCat.length) {
-      posicionCat = 0;
+    for (let i = 0; i < filas; i++) {
+        arrayPosiciones[i] = []
     }
-    arrayPosiciones[0][j] = arrayCat[posicionCat]
-    posicionCat++;
 
-  }
+    for (let j = 0; j < columnnas; j++) {
 
-  for (let j = 1; j < filas; j++) {
-    if (posicionCat == arrayCat.length) {
-      posicionCat = 0;
+        if (posicionCat == arrayCat.length) {
+            posicionCat = 0;
+        }
+        arrayPosiciones[0][j] = arrayCat[posicionCat]
+        posicionCat++;
+
     }
-    arrayPosiciones[j][columnnas - 1] = arrayCat[posicionCat]
-    posicionCat++
-  }
 
-  for (let j = columnnas - 2; j >= 0; j--) {
-    if (posicionCat == arrayCat.length) {
-      posicionCat = 0;
+    for (let j = 1; j < filas; j++) {
+        if (posicionCat == arrayCat.length) {
+            posicionCat = 0;
+        }
+        arrayPosiciones[j][columnnas - 1] = arrayCat[posicionCat]
+        posicionCat++
     }
-    arrayPosiciones[filas - 1][j] = arrayCat[posicionCat]
-    posicionCat++
 
-  }
+    for (let j = columnnas - 2; j >= 0; j--) {
+        if (posicionCat == arrayCat.length) {
+            posicionCat = 0;
+        }
+        arrayPosiciones[filas - 1][j] = arrayCat[posicionCat]
+        posicionCat++
 
-  for (let j = filas - 2; j > 0; j--) {
-    if (posicionCat == arrayCat.length) {
-      posicionCat = 0;
     }
-    arrayPosiciones[j][0] = arrayCat[posicionCat]
-    posicionCat++
-  }
 
-  for (let fila = 0; fila <= filas - 1; fila++) {
-    for (let columna = 0; columna <= columnnas - 1; columna++) {
-      const casilla = document.createElement('div');
-      const icono = document.createElement('i');
-
-      casilla.className = arrayPosiciones[fila][columna];
-
-      switch (casilla.className) {
-        case 'H':
-          icono.className = "fa fa-university";
-          break;
-        case 'C':
-          icono.className = "fa fa-flask";
-          break;
-        case 'A':
-          icono.className = "fa fa-map";
-          break;
-        case 'E':
-          icono.className = "fa fa-film";
-          break;
-        case 'D':
-          icono.className = "fa fa-bicycle";
-          break;
-        case 'G':
-          icono.className = "fa fa-map";
-          break;
-      }
-      casilla.appendChild(icono);
-      tablero.appendChild(casilla);
+    for (let j = filas - 2; j > 0; j--) {
+        if (posicionCat == arrayCat.length) {
+            posicionCat = 0;
+        }
+        arrayPosiciones[j][0] = arrayCat[posicionCat]
+        posicionCat++
     }
-  }
+
+    for (let fila = 0; fila <= filas - 1; fila++) {
+        for (let columna = 0; columna <= columnnas - 1; columna++) {
+            const casilla = document.createElement('div');
+            const icono = document.createElement('i');
+
+            casilla.className = arrayPosiciones[fila][columna];
+
+            switch (casilla.className) {
+                case 'H':
+                    icono.className = "fa fa-university";
+                    break;
+                case 'C':
+                    icono.className = "fa fa-flask";
+                    break;
+                case 'A':
+                    icono.className = "fa fa-map";
+                    break;
+                case 'E':
+                    icono.className = "fa fa-film";
+                    break;
+                case 'D':
+                    icono.className = "fa fa-bicycle";
+                    break;
+                case 'G':
+                    icono.className = "fa fa-map";
+                    break;
+            }
+            casilla.appendChild(icono);
+            tablero.appendChild(casilla);
+        }
+    }
 }
-
+const filas = 10;
+const columnas = 10;
 generarTablero(filas, columnas);
 //-----------------------------------DADO---------------------------------------------------//
 const cube = document.querySelector('.cube');
@@ -113,250 +113,177 @@ const time = 2;
 
 //cube.addEventListener('click', () => {
 function girarDado() {
-  cube.style.transition = '';
-  cube.style.transform = `translateY(40px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
-  setTimeout(() => {
-    cube.style.transition = `transform ${time}s`;
-    const randomValue = Math.floor((Math.random() * 6) + 1);
-    console.log(`randomValue: ${randomValue}`);
+    cube.style.transition = '';
+    cube.style.transform = `translateY(40px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+    setTimeout(() => {
+        cube.style.transition = `transform ${time}s`;
+        const randomValue = Math.floor((Math.random() * 6) + 1);
 
-    switch (randomValue) {
-      case 1:
-        cube.style.transform = `translateY(40px) rotateX(3600deg) rotateY(3600deg) rotateZ(3600deg)`;
-        tirar(randomValue);
-        break;
-      case 2:
-        cube.style.transform = `translateY(40px) rotateX(4410deg) rotateY(3600deg) rotateZ(3600deg)`;
-        tirar(randomValue);
-        break;
-      case 3:
-        cube.style.transform = `translateY(40px) rotateX(3600deg) rotateY(4410deg) rotateZ(3600deg)`;
-        tirar(randomValue);
-        break;
-      case 4:
-        cube.style.transform = `translateY(40px) rotateX(3600deg) rotateY(2430deg) rotateZ(3600deg)`;
-        tirar(randomValue);
-        break;
-      case 5:
-        cube.style.transform = `translateY(40px) rotateX(2430deg) rotateY(3600deg) rotateZ(3600deg)`;
-        tirar(randomValue);
-        break;
-      case 6:
-        cube.style.transform = `translateY(40px) rotateX(3600deg) rotateY(1980deg) rotateZ(3600deg)`;
-        tirar(randomValue);
-        break;
-    };
-  }, time * 1);
+        switch (randomValue) {
+            case 1:
+                cube.style.transform = `translateY(40px) rotateX(3600deg) rotateY(3600deg) rotateZ(3600deg)`;
+                tirar(randomValue);
+                break;
+            case 2:
+                cube.style.transform = `translateY(40px) rotateX(4410deg) rotateY(3600deg) rotateZ(3600deg)`;
+                tirar(randomValue);
+                break;
+            case 3:
+                cube.style.transform = `translateY(40px) rotateX(3600deg) rotateY(4410deg) rotateZ(3600deg)`;
+                tirar(randomValue);
+                break;
+            case 4:
+                cube.style.transform = `translateY(40px) rotateX(3600deg) rotateY(2430deg) rotateZ(3600deg)`;
+                tirar(randomValue);
+                break;
+            case 5:
+                cube.style.transform = `translateY(40px) rotateX(2430deg) rotateY(3600deg) rotateZ(3600deg)`;
+                tirar(randomValue);
+                break;
+            case 6:
+                cube.style.transform = `translateY(40px) rotateX(3600deg) rotateY(1980deg) rotateZ(3600deg)`;
+                tirar(randomValue);
+                break;
+        };
+    }, time * 1);
 
 }
-
-var posicionJugadorX = 20;
-const ficha = document.getElementById("player");
-var tablero = document.getElementById("contenedor");
-
-const salto = 60;
-function tirar(numeroCasillas) {
-
-  console.log("metodo0")
-
-  if (posicionJugadorX < salto * (columnas - 1)) {
-    console.log("metodo1")
-    moveD(numeroCasillas);
-  }
-  if (posicionJugadorX >= salto * (columnas - 1)) {
-    console.log("metodo2")
-    moveAb(numeroCasillas);
-  }
-  /*else{
-    if (pos >= numeroCasillas*salto) {
-      console.log("ifStop")
-      clearInterval(id)
-    } else {
-      console.log("ifSuma")
-      posicionJugadorX++;
-      pos++;
-      auxNumCasillas--;
-      ficha.style.right = posicionJugadorX + "px";
-      ficha.style.left = posicionJugadorX + "px";
-    }
-  }*/
-}
-function moveD(numeroCasillas) {
-  let id = null;
-  let pos = 0;
-  let auxNumCasillas = numeroCasillas * salto;
-  clearInterval(id);
-  id = setInterval(frame, 5);
-  function frame() {
-    if (posicionJugadorX >= salto * (columnas - 1)) {
-      moveAb(Math.round(auxNumCasillas / 60));
-      console.log(Math.round(auxNumCasillas / 60));
-      clearInterval(id);
-    }
-    if (posicionJugadorX < salto * (columnas - 1)) {
-      if (pos >= numeroCasillas * salto) {
-        console.log("ifStop")
-        clearInterval(id)
-      } else {
-        console.log("ifSuma")
-        posicionJugadorX++;
-        pos++;
-        auxNumCasillas--;
-        ficha.style.right = posicionJugadorX + "px";
-        ficha.style.left = posicionJugadorX + "px";
-      }
-    }
-  }
-}
+let posicionJugadorX = 0;
 let posicionJugadorY = 0;
+let posicionJugadorXx = 0;
+let posicionJugadorYy = 0;
+const ficha = document.getElementById("player");
+const tamanoSalto = 50;
+//const tamanoCasilla=50
 
-function moveAb(numeroCasillas) {
-  let id = null;
-  let pos = 0;
-  let salto = 60;
-  let auxNumCasillas = numeroCasillas * salto;
-  clearInterval(id);
-  id = setInterval(frame, 5);
-
-  function frame() {
-    if (posicionJugadorY < salto * (filas - 2)) {
-      if (pos >= numeroCasillas * salto) {
-        console.log("Stop vertical")
-        clearInterval(id)
-      } else {
-        pos++;
-        posicionJugadorY++;
-        auxNumCasillas--;
-        ficha.style.bottom = posicionJugadorY + "px";
-        ficha.style.top = posicionJugadorY + "px";
-      }
-    }
-  }
-}
-let auxJugadorPosicionX = posicionJugadorX;
-function moveI(numeroCasillas) {
-  let id = null;
-  const elem = document.getElementById("player");
-
-  clearInterval(id);
-  id = setInterval(frame, 5);
-
-  function frame() {
-    if (posicionJugadorX <= 0) {
-      clearInterval(id);
-      //moveArr();
+function tirar(numeroCasillas) {
+    if (posicionJugadorX < tamanoSalto * (columnas - 1)) {
+        console.log("metodo1");
+        moveDerecha(numeroCasillas);
+    } else if (posicionJugadorY < tamanoSalto * (filas - 1)) {
+        console.log("metodo2");
+        moveTb(numeroCasillas);
+    } else if (posicionJugadorX >= tamanoSalto * (columnas - 1)&&(posicionJugadorY >= tamanoSalto * (filas - 1))) {
+        console.log("metodo3");
+        moveIzquierda(numeroCasillas);
     } else {
-      pos--;
-      auxJugadorPosicionX--;
-      elem.style.bottom = posicionJugadorX + "px";
-      elem.style.left = posicionJugadorX + "px";
+        console.log("metodo4");
+        moveBt(numeroCasillas);
     }
-  }
 }
 
-//---------- ANIMACIONNNNNNNNN----------------------//
-/*
-<!DOCTYPE html>
-<html>
-<style>
-#container {
-  width: 400px;
-  height: 400px;
-  position: relative;
-  background: black;
-}
-#animate {
-  width: 50px;
-  height: 50px;
-  position: absolute;
-  background-color: grey;
-}
-</style>
-<body>
-
-<p><button onclick="moveA()">Click Me</button></p> 
-
-<div id ="container">
-  <div id ="animate"></div>
-</div>
-
-<script>
-function moveA() {
-  let id = null;
-  const elem = document.getElementById("animate");   
-  let pos = 0;
-  clearInterval(id);
-  id = setInterval(frame, 5);
-  
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id)
-      moveAb();
-    } else {
-      pos++; 
-      elem.style.right = pos + "px"; 
-      elem.style.left = pos + "px"; 
+function moveDerecha(numeroCasillas) {
+    let id = null;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    let pos = 0;
+    let auxNumCasillas = numeroCasillas * tamanoSalto;
+    function frame() {
+        if (posicionJugadorX >= tamanoSalto * (columnas - 1)) {
+            moveTb(Math.round(auxNumCasillas / tamanoSalto));
+            clearInterval(id);
+        }
+        if (posicionJugadorX < tamanoSalto * (columnas - 1)) {
+            if (pos >= numeroCasillas * tamanoSalto) {
+                console.log("moveDerecha saltos completados")
+                clearInterval(id);
+            } else {
+                posicionJugadorX++;
+                pos++;
+                auxNumCasillas--;
+                ficha.style.left = posicionJugadorX + "px";
+            }
+        }
     }
-  }
 }
-function moveAb() {
-  let id = null;
-  const elem = document.getElementById("animate");   
-  let pos = 0;
-  clearInterval(id);
-  id = setInterval(frame, 5);
-  
-  function frame() {
-    if (pos == 350) {
-     clearInterval(id);
-      moveI()
-    } else {
-      pos++; 
-      elem.style.bottom = pos + "px"; 
-      elem.style.top = pos + "px"; 
-    }
-  }
-}
-function moveI() {
-  let id = null;
-  const elem = document.getElementById("animate");   
-  let pos = 350;
-  clearInterval(id);
-  id = setInterval(frame, 5);
-  
-  function frame() {
-    if (pos == 0) {
-     clearInterval(id);
-      moveArr();
-    } else {
-      pos--; 
-      elem.style.bottom = pos + "px"; 
-      elem.style.left = pos + "px"; 
-    }
-  }
-}
-function moveArr() {
-  let id = null;
-  const elem = document.getElementById("animate");   
-  let pos = 350;
-  clearInterval(id);
-  id = setInterval(frame, 5);
-  	
-  function frame() {
-    if (pos == 0) {
-     clearInterval(id);
 
-    } else {
-      pos--; 
-     
-      elem.style.top = pos + "px"; 
-      elem.style.bottom = pos + "px"; 
+function moveTb(numeroCasillas) {
+    let idTb = null;
+    clearInterval(idTb);
+    idTb = setInterval(frameAb, 5);
+    let pos = 0;
+    let auxNumCasillas = numeroCasillas * tamanoSalto;
+    function frameAb() {
+        if (posicionJugadorY >= tamanoSalto * (filas - 1)) {
+            moveIzquierda(Math.round(auxNumCasillas / tamanoSalto));
+            clearInterval(idTb);
+        }
+        if (posicionJugadorY < tamanoSalto * (filas - 1)) {
+            if (pos >= numeroCasillas * tamanoSalto) {
+                clearInterval(idTb);
+            } else {
+                posicionJugadorY++;
+                pos++;
+                auxNumCasillas--;
+                ficha.style.top = posicionJugadorY + "px";
+            }
+        }
     }
-  }
 }
-</script>
+let primeraI = true
+function moveIzquierda(numeroCasillas) {
+    if (primeraI) {
+        posicionJugadorXx =posicionJugadorX;
+        primeraI = false
+    }
+    let idIzq = null;
+    let posIzq = 0;
+    clearInterval(idIzq);
+    idIzq = setInterval(frameIzq, 5);
+    let auxNumCasillas = numeroCasillas * tamanoSalto;
 
-</body>
-</html>
+    function frameIzq() {
+       
+        if (posicionJugadorXx == 0) {
+            moveBt(Math.round(auxNumCasillas / tamanoSalto))
+            clearInterval(idIzq);
+        }
+        if (posicionJugadorXx > 0) {
+            if (posIzq >= numeroCasillas * tamanoSalto) {
+                clearInterval(idIzq);
+            } else {
+                posIzq++;
+                posicionJugadorXx--;
+                auxNumCasillas--;
+                ficha.style.left = posicionJugadorXx + "px";
+            }
+        }
+    }
+}
+let primeraD = true
+function moveBt(numeroCasillas) {
+    let auxNumCasillas = numeroCasillas * tamanoSalto;
+    if (primeraD) {
+        posicionJugadorYy = posicionJugadorY;
+        primeraD = false
+    }
+    let id = null;
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
 
-*/
+    function frame() {
+        if (posicionJugadorYy == 0) {
+            posicionJugadorX = 0;
+            posicionJugadorY = 0;
+            posicionJugadorXx = 0;
+            posicionJugadorYy = 0;
+            primeraD=true;
+            primeraI=true;
+
+            moveDerecha(Math.round(auxNumCasillas / tamanoSalto))
+            clearInterval(id);
+        }
+        if(posicionJugadorYy>0){
+            if (pos >= numeroCasillas * tamanoSalto) {
+                console.log("moveBt saltos completados")
+                clearInterval(id);
+            } else {
+                posicionJugadorYy--;
+                pos++;
+                auxNumCasillas--;
+                ficha.style.top = posicionJugadorYy + "px";
+            }
+        }
+    }
+}
